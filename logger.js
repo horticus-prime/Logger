4f74fbd7fe45b5f555d52cdb91fee808d1ff5a35
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-24 16:38:01 -0700

    Initialize hello world

---
+++ logger.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Setup socket.io-client
+const io = require('socket.io-client');
+
+// connect to server.js
+const socket = io.connect('http://localhost:3005');
+
+let moistureSensor = (message) => {
+  console.log(message);
+};
+
+let reqData = (message) => {
+  console.log(message);
+};
+
+let helloWorld = () => {
+  return 'Hello World!';
+};
+
+console.log(helloWorld());
+
+// Listen for file-save event
+socket.on('moisture-data', moistureSensor);
+socket.on('req-data', reqData);
