8ae0ae51ec75b77f27f500fcb805450cd7c71f96
Chloie Parsons <chloieparsons@gmail.com>
2019-06-26 10:43:21 -0700

    JS documentation added to logger.js

--- logger.js
+++ logger.js
@@ -2,26 +2,44 @@
 
 require('dotenv').config();
 
-// Setup socket.io-client
 const io = require('socket.io-client');
 
-// connect to server.js
+/**
+ * @const {} - connects Logger.js to Server.js through SOCKET
+ */
+
 const socket = io.connect(process.env.SOCKET);
 
+/**
+ * 
+ * @param {string} message - console logs message from the moisture sensor
+ */
 let moistureSensor = (message) => {
   console.log(message);
 };
 
+/**
+ * 
+ * @param {string} message - console logs message from reqData
+ */
 let reqData = (message) => {
   console.log(message);
 };
 
+/**
+ * @param {string} message - returns 'Hello World!' as a test of functionality
+ */
 let helloWorld = () => {
   return 'Hello World!';
 };
 
 console.log(helloWorld());
 
-// Listen for file-save event
+/**
+ * @param {string} moisture-data - listens for file-save event from moistureSensor
+ * 
+ * @param {string} req-data - listens for file-save event from reqData
+ */
+
 socket.on('moisture-data', moistureSensor);
 socket.on('req-data', reqData);
